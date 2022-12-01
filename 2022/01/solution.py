cc8c2545e87cbbb8554daac8dc9806826e1d4273
Cade Cuddy <cadecuddy@gmail.com>
2022-12-01 00:43:12 -0500

    add day 1 solution

--- 2022/01/solution.py
+++ 2022/01/solution.py
@@ -1,18 +1,14 @@
-from aocd import get_data, submit
-d, y = 1, 2021
+from aocd import submit
+day, year = 1, 2022
+
+raw = open('input.txt').read()
 
 
 def part_one():
-    data = get_data(day=d, year=y)
-    ans = 1195
-
-    return ans
+    return max([sum([int(x) for x in y.split()]) for y in raw.split('\n\n')])
 
 def part_two():
-    data = get_data(day=d, year=y)
-    ans = 1235
+    return sum(sorted([sum([int(x) for x in y.split()]) for y in raw.split('\n\n')])[-3:])
 
-    return ans
-
-submit(part_one(), part="a", day=d, year=y)
-submit(part_two(), part="b", day=d, year=y)
+submit(part_one(), part="a", day=day, year=year)
+submit(part_two(), part="b", day=day, year=year)
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 All my Advent of Code Solutions
 
 ## 2022
-Begins shortly :)
+* [Day 1](https://github.com/cadecuddy/AoC/tree/main/2022/01)
