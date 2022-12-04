175753ad695498615931efd0e8c5fad54dbe9fd0
Cade Cuddy <cadecuddy@gmail.com>
2022-12-04 00:40:22 -0500

    add day 4 solution

--- 2022/04/solution.py
+++ 2022/04/solution.py
@@ -1,18 +1,24 @@
-from aocd import get_data, submit
-d, y = 1, 2021
-
+d = [line.rstrip() for line in open("input.txt")]
 
 def part_one():
-    data = get_data(day=d, year=y)
-    ans = 1195
-
-    return ans
+    t = 0
+    for l in d:
+        s = l.split(",")
+        p1, p2 = [int(x) for x in s[0].split("-")], [int(x) for x in s[1].split("-")]
+        # p2 is entirely inside of p1 or vice versa
+        if (p1[0] <= p2[0] and p1[1] >= p2[1]) or (p2[0] <= p1[0] and p2[1] >= p1[1]):
+            t += 1
+    return t
 
 def part_two():
-    data = get_data(day=d, year=y)
-    ans = 1235
+    t = 0
+    for l in d:
+        s = l.split(",")
+        p1, p2 = [int(x) for x in s[0].split("-")], [int(x) for x in s[1].split("-")]
+        # general overlap | p2 start inside of p1, etc.
+        if p1[0] <= p2[0] <= p1[1] or p2[0] <= p1[0] <= p2[1]:
+            t += 1
+    return t
 
-    return ans
-
-submit(part_one(), part="a", day=d, year=y)
-submit(part_two(), part="b", day=d, year=y)
+print(part_one())
+print(part_two())
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ All my Advent of Code Solutions
 * [Day 1](https://github.com/cadecuddy/AoC/tree/main/2022/01)
 * [Day 2](https://github.com/cadecuddy/AoC/tree/main/2022/02)
 * [Day 3](https://github.com/cadecuddy/AoC/tree/main/2022/03)
+* [Day 4](https://github.com/cadecuddy/AoC/tree/main/2022/04)
 
 ## 2021 - Rust
 _[Haven't started yet]_
