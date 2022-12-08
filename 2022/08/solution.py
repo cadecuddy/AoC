e1d476b04462333608aec531d5cf3c5f3a95e8c9
Cade Cuddy <cadecuddy@gmail.com>
2022-12-08 01:27:29 -0500

    add day 8 solution

--- 2022/08/solution.py
+++ 2022/08/solution.py
@@ -1,18 +1,67 @@
-from aocd import get_data, submit
-d, y = 1, 2021
-
+lines = [[int(x) for x in line.rstrip()] for line in open("input.txt")]
 
 def part_one():
-    data = get_data(day=d, year=y)
-    ans = 1195
+    # start w perimeter
+    visible = (len(lines) * 2) + (len(lines[0]) * 2 ) - 4
+    for r in range(1, len(lines) - 1):
+        for c in range(1, len(lines[0]) - 1):
+            current = lines[r][c]
+            
+            left = lines[r][:c]
+            right = lines[r][c + 1:]
+            top = [lines[i][c] for i in range(0, r)]
+            bottom = [lines[i][c] for i in range(r + 1, len(lines))]
 
-    return ans
+            if max(left) < current or max(right) < current or max(top) < current or max(bottom) < current:
+                visible += 1
+    return visible
 
 def part_two():
-    data = get_data(day=d, year=y)
-    ans = 1235
+    scenic = 0
+    # edges don't count since they're guarunteed at least a 0
+    for r in range(1, len(lines) - 1):
+        for c in range(1, len(lines[0]) - 1):
+            current = lines[r][c]
+            # count left
+            left = 0
+            for i in range(c, -1, -1):
+                if i == c: continue
+                if lines[r][i] < current:
+                    left += 1
+                else:
+                    left += 1
+                    break
+            # count right
+            right = 0
+            for i in range(c, len(lines[0])):
+                if i == c: continue
+                if i > len(lines[0]): break
+                if lines[r][i] < current:
+                    right += 1
+                else:
+                    right += 1
+                    break
+            # count top
+            top = 0
+            for i in range(r, -1, -1):
+                if i == r: continue
+                if i < 0: break
+                if lines[i][c] < current:
+                    top += 1
+                else:
+                    top += 1
+                    break
+            # count bottom
+            bottom = 0
+            for i in range(r + 1, len(lines)):
+                if i == r: continue
+                if lines[i][c] < current:
+                    bottom += 1
+                else:
+                    bottom += 1
+                    break
+            scenic = max(scenic, right * left * top * bottom)
+    return scenic
 
-    return ans
-
-submit(part_one(), part="a", day=d, year=y)
-submit(part_two(), part="b", day=d, year=y)
+print(part_one())
+print(part_two())
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ All my Advent of Code Solutions
 * [Day 5](https://github.com/cadecuddy/AoC/tree/main/2022/05/solution.py) - `2152/2306`
 * [Day 6](https://github.com/cadecuddy/AoC/tree/main/2022/06/solution.py) - `1456/1678`
 * [Day 7](https://github.com/cadecuddy/AoC/tree/main/2022/07/solution.py) - `1614/3046`
+* [Day 8](https://github.com/cadecuddy/AoC/tree/main/2022/08/solution.py) - `1848/1442`
 
 ## 2021 - Rust
 _[Haven't started yet]_
