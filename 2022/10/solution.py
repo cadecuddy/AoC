5a6114259925bc12cc9693b0b0d2c52d0381d136
Cade Cuddy <cadecuddy@gmail.com>
2022-12-10 01:02:09 -0500

    add day 10 solution

--- 2022/10/solution.py
+++ 2022/10/solution.py
@@ -1,18 +1,43 @@
-from aocd import get_data, submit
-d, y = 1, 2021
-
+lines = [line.rstrip() for line in open('input.txt')]
 
 def part_one():
-    data = get_data(day=d, year=y)
-    ans = 1195
-
+    strengths = set([20, 60, 100, 140, 180, 220])
+    x = 1
+    ans = 0
+    cycle = 0
+    for line in lines:
+        command = line.split(' ')[0]
+        for _ in range(1 if command == "noop" else 2):
+            cycle += 1
+            if cycle in strengths:
+                ans += cycle * x
+        if command == "addx":
+            x += int(line.split(' ')[1])
     return ans
 
 def part_two():
-    data = get_data(day=d, year=y)
-    ans = 1235
+    cycle = 0
+    sprite = 1
+    crt = []
+    currentrow = ""
+    for line in lines:
+        command = line.split(' ')[0]
+        for _ in range(1 if command == 'noop' else 2):
+            cycle += 1
+            if len(currentrow) in range(sprite - 1, sprite + 2):
+                currentrow += "#"
+            else:
+                currentrow += " "
+            if len(currentrow) == 40:
+                crt.append(currentrow)
+                currentrow = ""
+        if command == 'addx':
+            sprite += int(line.split(' ')[1])
 
-    return ans
+    for x in crt:
+        for y in x:
+            print(y, end = '')
+        print()
 
-submit(part_one(), part="a", day=d, year=y)
-submit(part_two(), part="b", day=d, year=y)
+print(part_one())
+part_two()
