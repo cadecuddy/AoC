0924b239a6d5132d2434e77cc4c0ff8f9ea8566b
Cade Cuddy <cadecuddy@gmail.com>
2022-12-12 00:51:07 -0500

    add day 12 solution

--- 2022/12/solution.py
+++ 2022/12/solution.py
@@ -1,18 +1,40 @@
-from aocd import get_data, submit
-d, y = 1, 2021
+from collections import deque
+lines = [[x for x in line.rstrip()] for line in open("input.txt")]
 
+start, end = None, None
+starts = []
+for r, row in enumerate(lines):
+    for c, col in enumerate(row):
+        if col == "S":
+            start = (r, c)
+            starts.append((r, c))
+            lines[r][c] = "a"
+        elif col == "E":
+            end = (r, c)
+            lines[r][c] = "z"
+        elif lines[r][c] == "a":
+            starts.append((r, c))
+
+def bfs(start, end, mode):
+    queue = deque([(start, 0) for start in starts] if mode == 2 else [(start, 0)])
+    visited = set()
+    while queue:
+        (r, c), steps = queue.popleft()
+        if (r, c) == end:
+            return steps
+        if (r, c) in visited:
+            continue
+        visited.add((r, c))
+        for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < len(lines) and 0 <= nc < len(lines[nr]) and ord(lines[nr][nc]) - ord(lines[r][c]) <= 1:
+                queue.append(((nr, nc), steps + 1))
 
 def part_one():
-    data = get_data(day=d, year=y)
-    ans = 1195
-
-    return ans
+    return bfs(start, end, 1)
 
 def part_two():
-    data = get_data(day=d, year=y)
-    ans = 1235
+    return bfs(start, end, 2)
 
-    return ans
-
-submit(part_one(), part="a", day=d, year=y)
-submit(part_two(), part="b", day=d, year=y)
+print(part_one())
+print(part_two())
--- README.md
+++ README.md
@@ -13,6 +13,7 @@ All my Advent of Code Solutions, usually cleaned up after the inital rush/grind 
 * [Day 9](https://github.com/cadecuddy/AoC/tree/main/2022/09/solution.py) - `6579/5953`
 * [Day 10](https://github.com/cadecuddy/AoC/tree/main/2022/10/solution.py) - `3062/4484`
 * [Day 11](https://github.com/cadecuddy/AoC/tree/main/2022/11/) - `4998/2926`
+* [Day 12](https://github.com/cadecuddy/AoC/tree/main/2022/12/solution.py) - `1652/1698`
 
 ## 2021 - Rust
 _[Haven't started yet]_
